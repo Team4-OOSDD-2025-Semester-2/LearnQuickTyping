6d08810be4d42624b0b065c0b3da7ccf43101670
SparkleNL <ajansen1608@gmail.com>
2025-11-18 17:10:59 +0100

    Commit 2 - Added entry field and label to display a random (hardcoded) word.

--- LearnQuickTyping/LearnQuickTyping.App/Views/Main.xaml.cs
+++ LearnQuickTyping/LearnQuickTyping.App/Views/Main.xaml.cs
@@ -2,8 +2,21 @@ namespace LearnQuickTyping.App.Views;
 
 public partial class Main : ContentPage
 {
-	public Main()
+	private readonly string [] words = new string []
+	{
+		"example", "typing", "quick", "learn", "keyboard", "practice", "speed", "accuracy", "challenge", "improve"
+	};
+
+    public Main()
 	{
 		InitializeComponent();
+		DisplayRandomWord();
 	}
+
+	private void DisplayRandomWord()
+		{
+		var random = new Random();
+		int index = random.Next(words.Length);
+		ExampleWord.Text = words[index];
+    }
 }
