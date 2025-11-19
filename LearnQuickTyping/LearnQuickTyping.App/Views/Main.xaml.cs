a2adb6382a3897497d201a05ebf967aaa9eba1e2
C0d3Br34k3rs2 <s1202384@student.windesheim.nl>
2025-11-19 18:01:15 +0100

    Implemented time taken for each word
    - Time difference between start of word and "enter" keypress
    -Display of time taken in seconds

--- LearnQuickTyping/LearnQuickTyping.App/Views/Main.xaml.cs
+++ LearnQuickTyping/LearnQuickTyping.App/Views/Main.xaml.cs
@@ -2,45 +2,77 @@ namespace LearnQuickTyping.App.Views;
 
 public partial class Main : ContentPage
 {
-	private readonly string [] PracticeWords = new string []
-	{
-		"example", "typing", "quick", "learn", "keyboard", "practice", "speed", "accuracy", "challenge", "improve"
-	};
+    private DateTime startTime;
+    private DateTime endTime;
+    private Boolean isTiming = false;
+
+    private readonly string[] PracticeWords = new string[]
+    {
+        "example", "typing", "quick", "learn", "keyboard", "practice", "speed", "accuracy", "challenge", "improve"
+    };
 
     public Main()
-	{
-		InitializeComponent();
-		DisplayRandomWord();
-	}
+    {
+        InitializeComponent();
+        DisplayRandomWord();
+    }
 
     // Display a random word from the list above
     private void DisplayRandomWord()
     {
-		var random = new Random();
-		int index = random.Next(PracticeWords.Length);
+        var random = new Random();
+        int index = random.Next(PracticeWords.Length);
         PracticeWord.Text = PracticeWords[index];
     }
+
+    #region Time calculations
+    // Start timing typing
+    private void StartTiming(object sender, TextChangedEventArgs e)
+    {
+        if (!isTiming)
+        {
+            startTime = DateTime.Now;
+            isTiming = true;
+        }
+    }
+
+    // Stop timing typing
+    TimeSpan StopTiming()
+    {
+        TimeSpan elapsedTime = TimeSpan.Zero;
+
+        if (isTiming)
+        {
+            endTime = DateTime.Now;
+            isTiming = false;
+            elapsedTime = endTime - startTime;
+        }
+        return elapsedTime;
+    }
+    #endregion
 
     // When the user presses Enter after typing
     private void OnEntryDone(object sender, EventArgs e)
     {
         var entry = (Entry)sender;
 
-		CompareStringsOnEnter();
+        CompareStringsOnEnter(StopTiming());
     }
 
     // Compare the input text with the example word
-    void CompareStringsOnEnter()
+    void CompareStringsOnEnter(TimeSpan timeTaken)
     {
-		if (InputText.Text == PracticeWord.Text)
-		{
-			Result.Text = "Correct!";
-			Result.TextColor = Colors.Green;
+        if (InputText.Text == PracticeWord.Text)
+        {
+            Result.Text = $"Correct! \r\nTime: {timeTaken.TotalSeconds:F2} seconds";
+            Result.TextColor = Colors.Green;
+
+            DisplayRandomWord();
         }
-		else
-		{
-			Result.Text = "Try Again!";
-			Result.TextColor = Colors.Red;
+        else
+        {
+            Result.Text = "Try Again!";
+            Result.TextColor = Colors.Red;
         }
         InputText.Text = string.Empty;
     }
