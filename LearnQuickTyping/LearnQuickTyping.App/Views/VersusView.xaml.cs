738a0c8df08f314f7beb7cb50abe9b4dba2bce99
S1211660 <s1211660@student.windesheim.nl>
2025-11-27 15:35:13 +0100

    feat: code behind versusview toegevoegd
    Code behind voor de versusview toegevoegd

--- LearnQuickTyping/LearnQuickTyping.App/Views/VersusView.xaml.cs
+++ LearnQuickTyping/LearnQuickTyping.App/Views/VersusView.xaml.cs
@@ -1,9 +1,50 @@
+using LearnQuickTyping.App.ViewModels;
+using LearnQuickTyping.Core.Models;
+
 namespace LearnQuickTyping.App.Views;
 
 public partial class VersusView : ContentPage
 {
-	public VersusView()
-	{
-		InitializeComponent();
-	}
+    private readonly VersusViewModel _viewModel;
+
+    public VersusView(VersusViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+
+        _viewModel.RequestLetterUpdate += UpdateLetterDisplay;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _viewModel.InitializeVersusCommand.Execute(null);
+    }
+
+    private void UpdateLetterDisplay(List<LetterStatus> statuses)
+    {
+        var formattedString = new FormattedString();
+
+        foreach (var letterStatus in statuses)
+        {
+            var span = new Span
+            {
+                Text = letterStatus.Character.ToString(),
+                FontSize = PracticeWordLabel.FontSize
+            };
+
+            span.TextColor = letterStatus.Status switch
+            {
+                Status.Correct => Colors.Green,
+                Status.Incorrect => Colors.Red,
+                Status.Pending => Colors.Gray,
+                _ => Colors.Black
+            };
+
+            formattedString.Spans.Add(span);
+        }
+
+        PracticeWordLabel.FormattedText = formattedString;
+    }
 }
