6f2e1ddf5ebb5807e67027e5b6a2532278521f73
S1211660 <s1211660@student.windesheim.nl>
2025-11-28 12:03:53 +0100

    feat: created service interface for service score
    Added interface for versusscoreservice

---
+++ LearnQuickTyping/LearnQuickTyping.Core/Interfaces/Services/IVersusScoreService.cs
@@ -0,0 +1,10 @@
+﻿using LearnQuickTyping.Core.Models;
+
+namespace LearnQuickTyping.Core.Interfaces.Services
+{
+    public interface IVersusScoreService
+    {
+        VersusResult CalculateResult(string playerName, string targetText, string typedText, TimeSpan timeTaken);
+        string DetermineWinner(VersusResult player1, VersusResult player2);
+    }
+}
