b135ff894674292e6f1371c31de24a0a6cca14df
S1211660 <s1211660@student.windesheim.nl>
2025-12-10 17:55:53 +0100

    feat: added SqliteConnectionFactory
    Added connectionfactory for connecting to the database

---
+++ LearnQuickTyping/LearnQuickTyping.Core.Data/Database/SqliteConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.Sqlite;
+using LearnQuickTyping.Core.Interfaces.Database;
+
+namespace LearnQuickTyping.Core.Data.Database;
+
+public class SqliteConnectionFactory : ISqliteConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public SqliteConnectionFactory(string dbPath)
+    {
+        _connectionString = new SqliteConnectionStringBuilder
+        {
+            DataSource = dbPath,
+            Mode = SqliteOpenMode.ReadWriteCreate,
+            ForeignKeys = true
+        }.ToString();
+    }
+
+    public async Task<SqliteConnection> CreateOpenConnectionAsync()
+    {
+        var conn = new SqliteConnection(_connectionString);
+        await conn.OpenAsync();
+        return conn;
+    }
+}
