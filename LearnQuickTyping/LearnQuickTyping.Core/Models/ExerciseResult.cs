3148d3009d6e0be316bba1c03699112581ce63f3
S1211660 <s1211660@student.windesheim.nl>
2025-12-10 17:43:56 +0100

    feat: added model for saving exerciseresults
    Added a model in order to save exerciseresults

---
+++ LearnQuickTyping/LearnQuickTyping.Core/Models/ExerciseResult.cs
@@ -0,0 +1,38 @@
+﻿namespace LearnQuickTyping.Core.Models;
+
+public class ExerciseResult
+{
+    public int Id { get; set; }
+
+    public string Date { get; set; } = string.Empty;
+
+    public string Time { get; set; } = string.Empty;
+
+    public double WordsPerMinute { get; set; }
+
+    public int Accuracy { get; set; }
+
+    public TimeSpan TimeTaken { get; set; }
+
+    public int Errors { get; set; }
+
+    public ExerciseType ExerciseType { get; set; }
+
+    public DifficultyLevel DifficultyLevel { get; set; }
+}
+
+public enum ExerciseType
+{
+    Word,
+    Text,
+    Versus,
+    Lyrics
+}
+
+public enum DifficultyLevel
+{
+    Beginner,
+    Intermediate,
+    Advanced,
+    Expert
+}
