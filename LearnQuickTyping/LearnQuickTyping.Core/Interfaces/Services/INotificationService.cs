9f470f1c454345a3f3c2ab77bb092a1d0d8e8faf
S1211660 <s1211660@student.windesheim.nl>
2025-12-11 11:54:43 +0100

    feat: added interface for saving results and showing notifications
    Added interfaces for new services

---
+++ LearnQuickTyping/LearnQuickTyping.Core/Interfaces/Services/INotificationService.cs
@@ -0,0 +1,7 @@
+﻿namespace LearnQuickTyping.Core.Interfaces.Services;
+
+public interface INotificationService
+{
+    Task ShowSuccessAsync(string message);
+    Task ShowErrorAsync(string message);
+}
