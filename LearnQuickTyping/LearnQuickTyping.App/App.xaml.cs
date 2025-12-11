52738b61031e6c89688e5b44b5035612ce87d0be
S1211660 <s1211660@student.windesheim.nl>
2025-12-11 10:57:35 +0100

    feat: initialise database
    Initialize database with application start

--- LearnQuickTyping/LearnQuickTyping.App/App.xaml.cs
+++ LearnQuickTyping/LearnQuickTyping.App/App.xaml.cs
@@ -1,16 +1,26 @@
-﻿namespace LearnQuickTyping.App
+﻿using LearnQuickTyping.Core.Data.Database;
+
+namespace LearnQuickTyping.App
 {
     public partial class App : Application
     {
-        public App()
+        private readonly SqliteSchemaMigrator _migrator;
+
+        public App(SqliteSchemaMigrator migrator)
         {
             InitializeComponent();
-
+            _migrator = migrator;
         }
+
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
         }
 
+        protected override async void OnStart()
+        {
+            base.OnStart();
+            await _migrator.MigrateAsync();
+        }
     }
 }
