0d15057a20f4b870e892cb83e58d01cb9b28ddd7
S1211660 <s1211660@student.windesheim.nl>
2025-12-13 12:27:08 +0100

    feat: added routing
    Can now go to progressview

--- LearnQuickTyping/LearnQuickTyping.App/AppShell.xaml.cs
+++ LearnQuickTyping/LearnQuickTyping.App/AppShell.xaml.cs
@@ -16,5 +16,6 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(Views.LyricsExercise), typeof(Views.LyricsExercise));
         Routing.RegisterRoute(nameof(Views.LyricsResult), typeof(Views.LyricsResult));
         Routing.RegisterRoute(nameof(Views.DifficultySelectView), typeof(Views.DifficultySelectView));
+        Routing.RegisterRoute(nameof(Views.ProgressPage), typeof(Views.ProgressPage));
     }
 }
